0c2e2b34051ebf48745b6f6a88df2dbb90ff9148
Bilal <bilal@berri.com>
2025-10-03 16:28:26 +0200

    Ajout : - Classe Commercial - Méthode AjouterNoteFrais pour un ajouter un objet NoteFrais dans la liste listNoteFrais - D'autres trucs

--- libCommerciaux/Commercial.cs
+++ libCommerciaux/Commercial.cs
@@ -2,6 +2,51 @@
 {
     public class Commercial
     {
-        
+        private string nom;
+        private string prenom;
+        private char categorie;
+        private int puissance;
+        private List<NoteFrais> listNoteFrais;
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+        public string Prenom
+        {
+            get { return prenom; }
+            set { prenom = value; }
+        }
+        public char Categorie
+        {
+            get { return categorie; }
+            set { categorie = value; }
+        }
+        public int Puissance
+        {
+            get { return puissance; }
+            set { puissance = value; }
+        }
+        public List<NoteFrais> ListNoteFrais
+        {
+            get { return listNoteFrais; }
+            set { listNoteFrais = value; }
+        }
+        public Commercial(string Prenom, string Nom, int Puissance, char Categorie)
+        {
+            nom = Nom;
+            prenom = Prenom;
+            categorie = Categorie;
+            puissance = Puissance;
+            listNoteFrais = new List<NoteFrais>();
+        }
+        public void AjouterNoteFrais(NoteFrais nf)
+        {
+            listNoteFrais.Add(nf);
+        }
+        public override string ToString()
+        {
+            return $"Nom : {nom} Prénom : {prenom} Puissance voiture : {puissance} Categorie : {categorie}";
+        }
     }
 }
---
+++ libCommerciaux/NoteFrais.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libCommerciaux
+{
+    public class NoteFrais
+    {
+        private int numero;
+        private DateTime date;
+        private double montant;
+        private bool rembourse;
+        private Commercial commercial;
+        //public int Numero
+        //{
+        //    get { return numero; }
+        //    set { numero = value; }
+        //}
+        //public DateTime Date
+        //{
+        //    get { return date; }
+        //    set { date = value; }
+        //}
+        //public bool Rembourse
+        //{
+        //    get { return rembourse; }
+        //    set { rembourse = value; }
+        //}
+        public double Montant
+        {
+            get { return Montant; }
+            set { Montant = value; }
+        }
+        public Commercial Commercial
+        {
+            get { return commercial; }
+            set { commercial = value; }
+        }
+        public NoteFrais(DateTime Date, Commercial Commercial)
+        {
+
+            this.date = Date;
+            this.commercial = Commercial;
+        }
+
+    }
+}
