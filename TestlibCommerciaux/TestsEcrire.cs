467ab1d3e7ff1a583c791b60353ac16024c66ba3
Bilal <bilal@berri.com>
2025-10-10 16:56:56 +0200

    Fait : - 2 nouveaux tests dans TestEcrire.cs dont 1 qui échoue

--- TestlibCommerciaux/TestsEcrire.cs
+++ TestlibCommerciaux/TestsEcrire.cs
@@ -20,5 +20,33 @@ namespace TestlibCommerciaux
             //Auditer
             Assert.AreEqual(2, c.getMesNoteFrais().Count);
         }
+
+        [TestMethod]
+        public void nbFraisNonRemboursesTest()
+        {
+            //Arranger
+            ServiceCommercial sc = new ServiceCommercial();
+            Commercial c0, c1;
+            NoteFrais f0, f1, f2, f3, f4;
+            c0 = new Commercial("Dupond", "Jean", 8, 'A');
+            c1 = new Commercial("Duval", "René", 6, 'A');
+
+            sc.ajouterCommercial(c0);
+            sc.ajouterCommercial(c1);
+
+            f0 = new NoteFrais(new DateTime(2022, 10, 12), c0);
+            f1 = new NoteFrais(new DateTime(2022, 10, 15), c0);
+            f2 = new NoteFrais(new DateTime(2022, 10, 18), c1);
+            f3 = new NoteFrais(new DateTime(2022, 10, 21), c1);
+            f4 = new NoteFrais(new DateTime(2022, 10, 25), c1);
+
+            //Agir
+            f1.setRembourse();
+            f3.setRembourse();
+            f3.setRembourse();
+
+            //Auditer
+            Assert.AreEqual(2, sc.nbFraisNonRembourses());
+        }
     }
 }
--- libCommerciaux/ServiceCommercial.cs
+++ libCommerciaux/ServiceCommercial.cs
@@ -8,6 +8,37 @@ namespace libCommerciaux
 {
     public class ServiceCommercial
     {
-        private 
+        private List<Commercial> lesCommerciaux;
+        public List<Commercial> LesCommerciaux
+        {
+            get { return lesCommerciaux; }
+            set { lesCommerciaux = value; }
+        }
+
+        public ServiceCommercial()
+        {
+            this.lesCommerciaux = new List<Commercial>();
+        }
+
+        public void ajouterCommercial(Commercial c)
+        {
+            lesCommerciaux.Add(c);
+        }
+
+        public int nbFraisNonRembourses()
+        {
+            int nb_frais = 0;
+            foreach (Commercial commercial in lesCommerciaux)
+            {
+                foreach (NoteFrais note in commercial.MesNotes)
+                {
+                    if (note.getEstRembourse() == false)
+                    {
+                        nb_frais += 1;
+                    }
+                }
+            }
+            return nb_frais;
+        }
     }
 }
