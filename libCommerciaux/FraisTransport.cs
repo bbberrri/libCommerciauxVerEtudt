c08f0d8f0c4613a1b9d3e1e6ce85e254614f619f
Bilal <bilal@berri.com>
2025-11-07 15:31:37 +0100

    Ajouts des classes FraisTransport, RepasMidi complétées et Nuite en cours

---
+++ libCommerciaux/FraisTransport.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libCommerciaux
+{
+    public class FraisTransport
+    {
+        private DateTime date;
+        private int distance;
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+        public int Distance
+        {
+            get { return distance; }
+            set { distance = value; }
+        }
+
+        public FraisTransport(DateTime Date, int Distance)
+        {
+            this.date = Date;
+            this.distance = Distance;
+        }
+    }
+}
---
+++ libCommerciaux/RepasMidi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libCommerciaux
+{
+    public class RepasMidi
+    {
+        private DateTime date;
+        private double facture;
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+        public double Facture
+        {
+            get { return facture; }
+            set { facture = value; }
+        }
+
+        public RepasMidi(DateTime Date, double Facture)
+        {
+            this.date = Date;
+            this.facture = Facture;
+        }
+    }
+}
